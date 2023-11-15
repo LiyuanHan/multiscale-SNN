8896f66e42fffaed51868e46b89f87d91561dcbd
Liyuan Han <50815237+LiyuanHan@users.noreply.github.com>
2023-11-15 17:00:36 +0800

    Update IZ_Task_BCI_pure.m

--- IZ_Task_BCI_pure.m
+++ IZ_Task_BCI_pure.m
@@ -12,7 +12,8 @@
 clear all
 close all
 clc
-data = load('data_for_BCI_RLS.mat') % final saved result : task-lorenz_1e-4_T50.mat
+% data = load('data_for_BCI_RLS.mat') % final saved result : task-lorenz_1e-4_T50.mat
+data = load('data_for_BCI_RLS-v2.mat') % final saved result : task-lorenz_1e-4_T50.mat
 data_t = linspace(0,48,length(data.position));
 time_win = 1;
 color_set = {[0,148,255]/255, [169,169,169]/255,...
